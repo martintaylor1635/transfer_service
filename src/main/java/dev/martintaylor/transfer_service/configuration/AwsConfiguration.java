7792c2003712efbadf0a8c0617e7b02a28a7a1d9
martintaylor1635 <martintaylor1635@gmail.com>
2024-03-22 18:22:37 +0000

    Added initial SQS client, refactored DynamoDbEnhancedClient bean declaration.

--- src/main/java/dev/martintaylor/transfer_service/configuration/AwsConfiguration.java
+++ src/main/java/dev/martintaylor/transfer_service/configuration/AwsConfiguration.java
@@ -3,17 +3,27 @@ package dev.martintaylor.transfer_service.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 @Configuration
 class AwsConfiguration {
     @Bean
     DynamoDbEnhancedClient dynamoDbEnhancedClient() {
-        return DynamoDbEnhancedClient.builder().build();
+        return DynamoDbEnhancedClient.builder()
+            .dynamoDbClient(DynamoDbClient.builder()
+                .build())
+            .build();
     }
 
     @Bean
     S3AsyncClient s3AsyncClient() {
         return S3AsyncClient.builder().build();
+    }
+
+    @Bean
+    SqsAsyncClient sqsAsyncClient() {
+        return SqsAsyncClient.builder().build();
     }
 }
